a38d8705494e95627cf52a0a43099f16e0c8aed1
Yemi Ogunleye <yemmy.akinyemmy@gmail.com>
2024-08-25 16:22:04 +0100

    commit 1

---
+++ courses/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
+    def _str_(self):
+        return self.summary
